5137c23ce1d5a05ffe6f084a2371c5edf2c1c80a
JackBaker1 <163625@chrome.escg.ac.uk>
2021-03-16 11:24:03 +0000

    Added in camera movement and bounds

---
+++ Assets/Script/Bounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+
+    private BoxCollider2D bounds;
+    private CamMovement theCamera;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounds = GetComponent<BoxCollider2D>();
+        theCamera = FindObjectOfType<CamMovement>();
+        theCamera.SetBounds(bounds);
+    }
+
+
+}
