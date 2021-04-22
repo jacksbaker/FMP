fa5128b890f6f9f3e73121b49844f8cffd0a33b5
JackBaker1 <163625@chrome.escg.ac.uk>
2021-04-22 16:13:07 +0100

    Made the menu and started to work on the level selector

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    public string levelToLoad = "LevelSelector";
+
+    public SceneFader sceneFader; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        sceneFader.FadeTo(levelToLoad);
+
+        
+        Debug.Log("Play");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
